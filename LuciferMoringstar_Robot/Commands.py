0f573dcb90024b56f62cd764c89f73e28f850189
Mallumoviezbot <98936426+Mallumoviezbot@users.noreply.github.com>
2022-02-04 16:07:51 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -84,7 +84,7 @@ async def start(bot, message):
                     f_caption = f"{files.file_name}"
                 buttons = [
                     [
-                        InlineKeyboardButton('🎖 DEPLOY YOURS 🎖', url=f'{TUTORIAL}')
+                        InlineKeyboardButton('🎖 Join channel 🎖', url=f'{https://t.me/mallumoviezchannel}')
                     ]
                     ]
                 await bot.send_cached_media(
