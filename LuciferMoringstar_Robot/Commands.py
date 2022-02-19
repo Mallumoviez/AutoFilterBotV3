5b41c5428d2ad92666926f2a521154cd497e67bc
Mallumoviezbot <98936426+Mallumoviezbot@users.noreply.github.com>
2022-02-19 17:06:54 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -49,7 +49,7 @@ async def start(bot, message):
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("📢 Join channel 📢", url=invite_link.invite_link)
+                                InlineKeyboardButton("📢 Join Here Acces 📢", url=invite_link.invite_link)
                             ],
                             [
                                 InlineKeyboardButton("🔄 Try Again", callback_data=f"checksub#{file_id}")
